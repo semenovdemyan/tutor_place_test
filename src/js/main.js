b2bcc2e3b8d7f410b3e5d89ec31997586cdea193
Demian Semenov <semenovdemyan@gmail.com>
2025-01-15 13:03:26 +0300

    upd

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
-document
-  .querySelector('.header__burger')
-  .addEventListener('click', function () {
-    document.querySelector('.header__nav').classList.toggle('open');
-    this.classList.toggle('open');
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerButton = document.querySelector('.header__burger'); // Кнопка бургер-меню
+  const nav = document.querySelector('.header__nav'); // Меню
+
+  // Если кнопка и меню существуют, добавляем обработчик события
+  if (burgerButton && nav) {
+    burgerButton.addEventListener('click', function () {
+      nav.classList.toggle('open'); // Добавляем/удаляем класс 'open'
+    });
+  }
+});
