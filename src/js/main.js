d0af6370f26b2a27a9eafc1e7a45c9d14934c509
Demian Semenov <semenovdemyan@gmail.com>
2025-01-17 01:35:58 +0300

    add modal triggers

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const modal = document.querySelector('#modal');
-  const openModalBtn = document.querySelector('.hero__btn.hero__btn--primary'); // кнопка для открытия
-  const closeModalBtn = document.querySelector('.modal__close-btn'); // кнопка для закрытия
-  const modalOverlay = document.querySelector('.modal-overlay'); // фон для затемнения и блюра
-  const modalForm = document.querySelector('#modalForm'); // форма внутри модалки
+  const modal = document.querySelector('.modal');
+  const openModalBtn = document.querySelector('.btn--modal-trigger');
+  const closeModalBtn = document.querySelector('.modal__close-btn');
+  const modalForm = document.querySelector('#modalForm');
+  let modalOpenedByTimer = false;
 
-  // Открытие модального окна
   openModalBtn.addEventListener('click', () => {
-    modal.style.display = 'block'; // отображаем модальное окно
-    modal.showModal(); // открытие диалога
-    modalOverlay.style.filter = 'blur(5px)'; // применяем блюр на фоне
+    modal.style.display = 'flex';
   });
 
-  // Закрытие модального окна
   closeModalBtn.addEventListener('click', () => {
-    modal.style.display = 'none'; // скрываем модальное окно
-    modal.close(); // закрытие диалога
-    modalOverlay.style.filter = ''; // убираем блюр с фона
+    modal.style.display = 'none';
   });
 
-  // Закрытие модального окна при клике вне области модалки
   window.addEventListener('click', (e) => {
-    if (e.target === modal && modalOverlay) {
-      modal.style.display = 'none'; // скрываем модальное окно
-      modal.close(); // закрытие диалога
-      modalOverlay.style.filter = ''; // убираем блюр с фона
+    if (e.target === modal) {
+      modal.style.display = 'none';
     }
   });
 
-  // Остановка прокрутки при отправке формы
   modalForm.addEventListener('submit', (e) => {
-    e.preventDefault(); // предотвращаем стандартное поведение формы (отправка данных и перезагрузка страницы)
-    alert('Форма отправлена!'); // тут можно добавить логику для отправки данных, если нужно
+    e.preventDefault();
+    alert('Форма отправлена!');
   });
+
+  function openModalByTimer() {
+    if (!modalOpenedByTimer) {
+      modal.style.display = 'flex';
+      modalOpenedByTimer = true;
+    }
+  }
+
+  setTimeout(openModalByTimer, 60000);
 });
