affed5810c6647a7ee8a9a4cec935b81ff6926bc
Demian Semenov <semenovdemyan@gmail.com>
2025-01-17 12:13:07 +0300

    final

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // Модальное окно
   const modal = document.querySelector('.modal');
   const openModalBtn = document.querySelector('.btn--modal-trigger');
   const closeModalBtn = document.querySelector('.modal__close-btn');
   const modalForm = document.querySelector('#modalForm');
+
   let modalOpenedByTimer = false;
 
   openModalBtn.addEventListener('click', () => {
@@ -24,6 +26,11 @@ document.addEventListener('DOMContentLoaded', () => {
     alert('Форма отправлена!');
   });
 
+  modalForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Форма отправлена!');
+  });
+
   function openModalByTimer() {
     if (!modalOpenedByTimer) {
       modal.style.display = 'flex';
@@ -31,5 +38,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  setTimeout(openModalByTimer, 60000);
+  setTimeout(openModalByTimer, 5000);
+
+  // Burger Menu
+  const burgerButton = document.querySelector('.header__burger');
+  const burgerNav = document.querySelector('.burger__nav');
+  const container = document.querySelector('.container');
+
+  burgerButton.addEventListener('click', () => {
+    burgerNav.classList.toggle('active');
+    container.classList.toggle('blurred');
+  });
+
+  window.addEventListener('click', (e) => {
+    if (
+      burgerNav.classList.contains('active') &&
+      !burgerNav.contains(e.target) &&
+      !burgerButton.contains(e.target)
+    ) {
+      burgerNav.classList.remove('active');
+      container.classList.remove('blurred');
+    }
+  });
 });
